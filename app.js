3fc90596323d2e96dba03dc7733ae629985f4764
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-03 12:39:36 -0500

    add arrays for suits and for card values

---
+++ app.js
@@ -0,0 +1,7 @@
+// an array containing the suits in a deck of cards
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+
+// an array containing all the possible values in a deck of cards
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+console.log(values, suits)
