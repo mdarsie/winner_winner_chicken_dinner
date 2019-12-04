da7850a3431734ac27ef7259375d0b5f669ea020
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-04 10:29:52 -0500

    an array of a deck of cards

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 // an array containing the suits in a deck of cards
-var suit = ['spades', 'diamonds', 'clubs', 'hearts'];
+let suit = ['spades', 'diamonds', 'clubs', 'hearts'];
 
 // an array containing all the possible values in a deck of cards
-var value = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+let value = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
 
-// a new array to hold combined suit and values
-var deckComplete =  Array();
+// an empty array for the deck that is a combination of suits and values
+var deck = new Array();
 
-// function combineArrays(){
-//     let deckComplete = Array();
-//     for(var i = 0; i < suit.length; i++){
-//         for(var x = 0; x < value.length; x++){
-        
+// // a class to hold both of the arrays!
+// class Card {
+//     constructor (suit, value) {
+//     this.suit = suit
+//     this.value = value
+//     }
 // }
-// console.log(Array(deckComplete))
+function getDeck(){
+    var deck = new Array();
+        for(var i = 0; i < suit.length; i++ ){
+            for(var x = 0; x < value.length; x++){
+                var card = (suit[i], value[x]);
+                    deck.push(card);
+            }
+        }
+    return deck;
+}
+console.log(deck)
+ 
+
+
+
+
+
+
+
+
+//const CircleType = new CircleType(document.getElementById('blackjackPay'))
+ //   circleType.radius(200).dir(-1);
--- assets/src/entry.js
+++
@@ -1,4 +0,0 @@
-import 'core-js/fn/array/from';
-import CircleType from './class';
-
-module.exports = CircleType;
