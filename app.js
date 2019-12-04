108c76366f08e80e8b92903f7c30038ec2f7a2b4
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-04 12:33:03 -0500

    create an array of a deck of cards

--- app.js
+++ app.js
@@ -4,27 +4,25 @@ let suit = ['spades', 'diamonds', 'clubs', 'hearts'];
 // an array containing all the possible values in a deck of cards
 let value = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
 
-// an empty array for the deck that is a combination of suits and values
+//an empty array for the deck that is a combination of suits and values
 var deck = new Array();
 
-// // a class to hold both of the arrays!
-// class Card {
-//     constructor (suit, value) {
-//     this.suit = suit
-//     this.value = value
-//     }
-// }
 function getDeck(){
     var deck = new Array();
         for(var i = 0; i < suit.length; i++ ){
             for(var x = 0; x < value.length; x++){
-                var card = (suit[i], value[x]);
+                var card = {suit : suit[i], value: value[x]}
                     deck.push(card);
             }
         }
     return deck;
 }
-console.log(deck)
+console.log(getDeck())
+// class Card {
+//     constructor(suit, value)
+//     this.suit= suit
+//     this.value= value
+// }
  
 
 
@@ -34,5 +32,11 @@ console.log(deck)
 
 
 
-//const CircleType = new CircleType(document.getElementById('blackjackPay'))
- //   circleType.radius(200).dir(-1);
+const circleType = new CircleType(document.getElementById('blackjackPay'))
+   circleType.radius(360).dir(-1);
+
+const circleTypeB = new CircleType(document.getElementById('dealerRule'))
+   circleTypeB.radius(360).dir(-1);
+
+const circleTypeC = new CircleType(document.getElementById('insurance'))
+   circleTypeC.radius(360).dir(-1);      
