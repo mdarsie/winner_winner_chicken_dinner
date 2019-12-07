0868e57f0c449d441caf9d572577da3caab2026a
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-07 15:26:27 -0500

    add values to array of cards, removed card png files

--- app.js
+++ app.js
@@ -11,81 +11,83 @@ let stayButton = document.getElementById("Stay");
 
 // add values to array
 let deck = [
-  { card: "\u{1F0A1}", value: 3 },
-  { card: "\u{1F0A2}", value: 4 },
-  { card: "\u{1F0A3}", value: 5 }
+  { card: "\u{1F0A1}", value: 11 },
+  { card: "\u{1F0A2}", value: 2 },
+  { card: "\u{1F0A3}", value: 3 },
+  { card: "\u{1F0A4}", value: 4 },
+  { card: "\u{1F0A5}", value: 5 },
+  { card: "\u{1F0A6}", value: 6 },
+  { card: "\u{1F0A7}", value: 7 },
+  { card: "\u{1F0A8}", value: 8 },
+  { card: "\u{1F0A9}", value: 9 },
+  { card: "\u{1F0AA}", value: 10 },
+  { card: "\u{1F0AB}", value: 10 },
+  { card: "\u{1F0AD}", value: 10 },
+  { card: "\u{1F0AE}", value: 10 },
+  { card: "\u{1F0B1}", value: 11 },
+  { card: "\u{1F0B2}", value: 2 },
+  { card: "\u{1F0B3}", value: 3 },
+  { card: "\u{1F0B4}", value: 4 },
+  { card: "\u{1F0B5}", value: 5 },
+  { card: "\u{1F0B6}", value: 6 },
+  { card: "\u{1F0B7}", value: 7 },
+  { card: "\u{1F0B8}", value: 8 },
+  { card: "\u{1F0B9}", value: 9 },
+  { card: "\u{1F0BA}", value: 10 },
+  { card: "\u{1F0BB}", value: 10 },
+  { card: "\u{1F0BD}", value: 10 },
+  { card: "\u{1F0BE}", value: 10 },
+  { card: "\u{1F0C1}", value: 11 },
+  { card: "\u{1F0C2}", value: 2 },
+  { card: "\u{1F0C3}", value: 3 },
+  { card: "\u{1F0C4}", value: 4 },
+  { card: "\u{1F0C5}", value: 5 },
+  { card: "\u{1F0C6}", value: 6 },
+  { card: "\u{1F0C7}", value: 7 },
+  { card: "\u{1F0C8}", value: 8 },
+  { card: "\u{1F0C9}", value: 9 },
+  { card: "\u{1F0CA}", value: 10 },
+  { card: "\u{1F0CB}", value: 10 },
+  { card: "\u{1F0CD}", value: 10 },
+  { card: "\u{1F0CE}", value: 10 },
+  { card: "\u{1F0D1}", value: 11 },
+  { card: "\u{1F0D2}", value: 2 },
+  { card: "\u{1F0D3}", value: 3 },
+  { card: "\u{1F0D4}", value: 4 },
+  { card: "\u{1F0D5}", value: 5 },
+  { card: "\u{1F0D6}", value: 6 },
+  { card: "\u{1F0D7}", value: 7 },
+  { card: "\u{1F0D8}", value: 8 },
+  { card: "\u{1F0D9}", value: 9 },
+  { card: "\u{1F0DA}", value: 10 },
+  { card: "\u{1F0DB}", value: 10 },
+  { card: "\u{1F0DD}", value: 10 },
+  { card: "\u{1F0DE}", value: 10 }
 ];
-//   "\u{1F0A4}",
-//   "\u{1F0A5}",
-//   "\u{1F0A6}",
-//   "\u{1F0A7}",
-//   "\u{1F0A8}",
-//   "\u{1F0A9}",
-//   "\u{1F0AA}",
-//   "\u{1F0AB}",
-//   "\u{1F0AD}",
-//   "\u{1F0AE}",
-//   "\u{1F0B1}",
-//   "\u{1F0B2}",
-//   "\u{1F0B3}",
-//   "\u{1F0B4}",
-//   "\u{1F0B5}",
-//   "\u{1F0B6}",
-//   "\u{1F0B7}",
-//   "\u{1F0B8}",
-//   "\u{1F0B9}",
-//   "\u{1F0BA}",
-//   "\u{1F0BB}",
-//   "\u{1F0BD}",
-//   "\u{1F0BE}",
-//   "\u{1F0C1}",
-//   "\u{1F0C2}",
-//   "\u{1F0C3}",
-//   "\u{1F0C4}",
-//   "\u{1F0C5}",
-//   "\u{1F0C6}",
-//   "\u{1F0C7}",
-//   "\u{1F0C8}",
-//   "\u{1F0C9}",
-//   "\u{1F0CA}",
-//   "\u{1F0CB}",
-//   "\u{1F0CD}",
-//   "\u{1F0CE}",
-//   "\u{1F0D1}",
-//   "\u{1F0D2}",
-//   "\u{1F0D3}",
-//   "\u{1F0D4}",
-//   "\u{1F0D5}",
-//   "\u{1F0D6}",
-//   "\u{1F0D7}",
-//   "\u{1F0D8}",
-//   "\u{1F0D9}",
-//   "\u{1F0DA}",
-//   "\u{1F0DB}",
-//   "\u{1F0DD}",
-//   "\u{1F0DE}"
-// ];
 
 //a function to shuffle the deck
 function shuffle(deck) {
   for (var i = 0; i < 500; i++) {
     var space1 = Math.floor(Math.random() * deck.length);
     var space2 = Math.floor(Math.random() * deck.length);
-    var holder = deck[space1];
+    var placeHolder = deck[space1];
     deck[space1] = deck[space2];
-    deck[space2] = holder;
+    deck[space2] = placeHolder;
   }
   return deck;
 }
 
 //a function to deal the dealer and player hands
 function dealHand() {
+  shuffle(deck);
   var playerHand = new Array();
   playerHand.push(deck[0]);
-  deck.splice(0, 1);
+  deck.shift(0);
+  playerHand.push(deck[0]);
+  deck.shift(0);
   return playerHand;
 }
-console.log(shuffle(deck));
+console.log(dealHand());
 
 //push cards to gameboard
 
@@ -97,11 +99,11 @@ console.log(shuffle(deck));
 
 //declare the winner or loser
 
-const circleType = new CircleType(document.getElementById("blackjackPay"));
-circleType.radius(360).dir(-1);
+// const circleType = new CircleType(document.getElementById("blackjackPay"));
+// circleType.radius(360).dir(-1);
 
-const circleTypeB = new CircleType(document.getElementById("dealerRule"));
-circleTypeB.radius(360).dir(-1);
+// const circleTypeB = new CircleType(document.getElementById("dealerRule"));
+// circleTypeB.radius(360).dir(-1);
 
-const circleTypeC = new CircleType(document.getElementById("insurance"));
-circleTypeC.radius(360).dir(-1);
+// const circleTypeC = new CircleType(document.getElementById("circle"));
+// circleTypeC.radius(360).dir(-1);
