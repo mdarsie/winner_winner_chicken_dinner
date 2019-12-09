b1da5bdb540e2f0ec0d9aa6b149f21d3e1afc07d
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-09 04:49:17 -0500

    calculate card value, hand value, winner and loser

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+const circleType = new CircleType(document.getElementById("blackjackPay"));
+circleType.radius(500).dir(-1);
+
+const circleTypeB = new CircleType(document.getElementById("softHit"));
+circleTypeB.radius(500).dir(-1);
+
 let playerHand = [];
 let dealerHand = [];
 let playerTotal = 0;
 let dealerTotal = 0;
+
+var newGameButton = document.getElementById("newGame");
+var hitButton = document.getElementById("hit");
+var stayButton = document.getElementById("stand");
+var dealAgainButton = document.getElementById("playAgain");
 
 //a function to determine the values of the cards
 function cardValue(card) {
@@ -44,10 +55,6 @@ function handTotal(hand) {
   }
   return handValue;
 }
-var newGameButton = document.getElementById("newGame");
-var hitButton = document.getElementById("hit");
-var stayButton = document.getElementById("stand");
-var dealAgainButton = document.getElementById("playAgain");
 
 //event listeners for game start, hit, stand, play again
 newGameButton.addEventListener("click", function() {
@@ -115,59 +122,6 @@ let deck = [
   "KD",
   "AD"
 ];
-//   { card: "\u{1F0A1}", value: 11 },
-//   { card: "\u{1F0A2}", value: 2 },
-//   { card: "\u{1F0A3}", value: 3 },
-//   { card: "\u{1F0A4}", value: 4 },
-//   { card: "\u{1F0A5}", value: 5 },
-//   { card: "\u{1F0A6}", value: 6 },
-//   { card: "\u{1F0A7}", value: 7 },
-//   { card: "\u{1F0A8}", value: 8 },
-//   { card: "\u{1F0A9}", value: 9 },
-//   { card: "\u{1F0AA}", value: 10 },
-//   { card: "\u{1F0AB}", value: 10 },
-//   { card: "\u{1F0AD}", value: 10 },
-//   { card: "\u{1F0AE}", value: 10 },
-//   { card: "\u{1F0B1}", value: 11 },
-//   { card: "\u{1F0B2}", value: 2 },
-//   { card: "\u{1F0B3}", value: 3 },
-//   { card: "\u{1F0B4}", value: 4 },
-//   { card: "\u{1F0B5}", value: 5 },
-//   { card: "\u{1F0B6}", value: 6 },
-//   { card: "\u{1F0B7}", value: 7 },
-//   { card: "\u{1F0B8}", value: 8 },
-//   { card: "\u{1F0B9}", value: 9 },
-//   { card: "\u{1F0BA}", value: 10 },
-//   { card: "\u{1F0BB}", value: 10 },
-//   { card: "\u{1F0BD}", value: 10 },
-//   { card: "\u{1F0BE}", value: 10 },
-//   { card: "\u{1F0C1}", value: 11 },
-//   { card: "\u{1F0C2}", value: 2 },
-//   { card: "\u{1F0C3}", value: 3 },
-//   { card: "\u{1F0C4}", value: 4 },
-//   { card: "\u{1F0C5}", value: 5 },
-//   { card: "\u{1F0C6}", value: 6 },
-//   { card: "\u{1F0C7}", value: 7 },
-//   { card: "\u{1F0C8}", value: 8 },
-//   { card: "\u{1F0C9}", value: 9 },
-//   { card: "\u{1F0CA}", value: 10 },
-//   { card: "\u{1F0CB}", value: 10 },
-//   { card: "\u{1F0CD}", value: 10 },
-//   { card: "\u{1F0CE}", value: 10 },
-//   { card: "\u{1F0D1}", value: 11 },
-//   { card: "\u{1F0D2}", value: 2 },
-//   { card: "\u{1F0D3}", value: 3 },
-//   { card: "\u{1F0D4}", value: 4 },
-//   { card: "\u{1F0D5}", value: 5 },
-//   { card: "\u{1F0D6}", value: 6 },
-//   { card: "\u{1F0D7}", value: 7 },
-//   { card: "\u{1F0D8}", value: 8 },
-//   { card: "\u{1F0D9}", value: 9 },
-//   { card: "\u{1F0DA}", value: 10 },
-//   { card: "\u{1F0DB}", value: 10 },
-//   { card: "\u{1F0DD}", value: 10 },
-//   { card: "\u{1F0DE}", value: 10 }
-// ];
 
 //a function to shuffle the deck
 function shuffle(deck) {
@@ -215,12 +169,26 @@ stayButton.addEventListener("click", function() {
 //a function to play the dealer's hand
 
 //declare the winner or loser
-
-const circleType = new CircleType(document.getElementById("blackjackPay"));
-circleType.radius(500).dir(-1);
-
-const circleTypeB = new CircleType(document.getElementById("softHit"));
-circleTypeB.radius(500).dir(-1);
-
-// const circleTypeC = new CircleType(document.getElementById("circle"));
-// circleTypeC.radius(360).dir(-1);
+function winnerWinner() {
+  dealerTotal = getHandValue(dealer);
+  playerTotal = getHandValue(player);
+  if (playerTotal === dealerTotal) {
+    alert("You tied!");
+  } else if (dealerTotal > playerTotal) {
+    if (dealerTotal === 21) {
+      alert("Loser, loser! Dealer has Blackjack!");
+    } else if (dealerTotal > 21) {
+      alert("Winner, winner!");
+    } else {
+      alert("Loser, loser! Dealer wins!");
+    }
+  } else {
+    if (playerTotal === 21) {
+      alert("BLACKJACK!");
+    } else if (playerTotal > 21) {
+      alert("Loser, loser! You busted!");
+    } else {
+      alert("Winner, winner!");
+    }
+  }
+}
