5d6c9b6f3dc69a7ecfa0f77ebe965a1bf8d36cc6
mdarsie <57684286+mdarsie@users.noreply.github.com>
2019-12-09 04:27:02 -0500

    change back to images for playing cards rather than drawing with unicode in the div

--- app.js
+++ app.js
@@ -3,8 +3,47 @@ let dealerHand = [];
 let playerTotal = 0;
 let dealerTotal = 0;
 
-//a function to determine the value of the hand and the parameters for winning or losing
+//a function to determine the values of the cards
+function cardValue(card) {
+  switch (card[1]) {
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      return parseInt(card[1]);
+    case "10":
+    case "J":
+    case "Q":
+    case "K":
+      return 10;
+    default:
+      return 11;
+  }
+}
 
+// a function to determine the value of a hand
+function handTotal(hand) {
+  var i = 0;
+  var aces = hand.filter(getAce);
+  var handValue = 0;
+
+  hand.forEach(function(card) {
+    return (handValue += cardValue(card));
+  });
+
+  if (aces.length !== 0) {
+    aces.forEach(function() {
+      if (handValue > 21) {
+        return (handValue -= 10);
+      }
+    });
+  }
+  return handValue;
+}
 var newGameButton = document.getElementById("newGame");
 var hitButton = document.getElementById("hit");
 var stayButton = document.getElementById("stand");
@@ -23,59 +62,112 @@ dealAgainButton.addEventListener("click", function() {
 
 // add values to array
 let deck = [
-  { card: "\u{1F0A1}", value: 11 },
-  { card: "\u{1F0A2}", value: 2 },
-  { card: "\u{1F0A3}", value: 3 },
-  { card: "\u{1F0A4}", value: 4 },
-  { card: "\u{1F0A5}", value: 5 },
-  { card: "\u{1F0A6}", value: 6 },
-  { card: "\u{1F0A7}", value: 7 },
-  { card: "\u{1F0A8}", value: 8 },
-  { card: "\u{1F0A9}", value: 9 },
-  { card: "\u{1F0AA}", value: 10 },
-  { card: "\u{1F0AB}", value: 10 },
-  { card: "\u{1F0AD}", value: 10 },
-  { card: "\u{1F0AE}", value: 10 },
-  { card: "\u{1F0B1}", value: 11 },
-  { card: "\u{1F0B2}", value: 2 },
-  { card: "\u{1F0B3}", value: 3 },
-  { card: "\u{1F0B4}", value: 4 },
-  { card: "\u{1F0B5}", value: 5 },
-  { card: "\u{1F0B6}", value: 6 },
-  { card: "\u{1F0B7}", value: 7 },
-  { card: "\u{1F0B8}", value: 8 },
-  { card: "\u{1F0B9}", value: 9 },
-  { card: "\u{1F0BA}", value: 10 },
-  { card: "\u{1F0BB}", value: 10 },
-  { card: "\u{1F0BD}", value: 10 },
-  { card: "\u{1F0BE}", value: 10 },
-  { card: "\u{1F0C1}", value: 11 },
-  { card: "\u{1F0C2}", value: 2 },
-  { card: "\u{1F0C3}", value: 3 },
-  { card: "\u{1F0C4}", value: 4 },
-  { card: "\u{1F0C5}", value: 5 },
-  { card: "\u{1F0C6}", value: 6 },
-  { card: "\u{1F0C7}", value: 7 },
-  { card: "\u{1F0C8}", value: 8 },
-  { card: "\u{1F0C9}", value: 9 },
-  { card: "\u{1F0CA}", value: 10 },
-  { card: "\u{1F0CB}", value: 10 },
-  { card: "\u{1F0CD}", value: 10 },
-  { card: "\u{1F0CE}", value: 10 },
-  { card: "\u{1F0D1}", value: 11 },
-  { card: "\u{1F0D2}", value: 2 },
-  { card: "\u{1F0D3}", value: 3 },
-  { card: "\u{1F0D4}", value: 4 },
-  { card: "\u{1F0D5}", value: 5 },
-  { card: "\u{1F0D6}", value: 6 },
-  { card: "\u{1F0D7}", value: 7 },
-  { card: "\u{1F0D8}", value: 8 },
-  { card: "\u{1F0D9}", value: 9 },
-  { card: "\u{1F0DA}", value: 10 },
-  { card: "\u{1F0DB}", value: 10 },
-  { card: "\u{1F0DD}", value: 10 },
-  { card: "\u{1F0DE}", value: 10 }
+  "2H",
+  "3H",
+  "4H",
+  "5H",
+  "6H",
+  "7H",
+  "8H",
+  "9H",
+  "10H",
+  "JH",
+  "QH",
+  "KH",
+  "AH",
+  "2S",
+  "3S",
+  "4S",
+  "5S",
+  "6S",
+  "7S",
+  "8S",
+  "9S",
+  "10S",
+  "JS",
+  "QS",
+  "KS",
+  "AS",
+  "2C",
+  "3C",
+  "4C",
+  "5C",
+  "6C",
+  "7C",
+  "8C",
+  "9C",
+  "10C",
+  "JC",
+  "QC",
+  "KC",
+  "AC",
+  "2D",
+  "3D",
+  "4D",
+  "5D",
+  "6D",
+  "7D",
+  "8D",
+  "9D",
+  "10D",
+  "JD",
+  "QD",
+  "KD",
+  "AD"
 ];
+//   { card: "\u{1F0A1}", value: 11 },
+//   { card: "\u{1F0A2}", value: 2 },
+//   { card: "\u{1F0A3}", value: 3 },
+//   { card: "\u{1F0A4}", value: 4 },
+//   { card: "\u{1F0A5}", value: 5 },
+//   { card: "\u{1F0A6}", value: 6 },
+//   { card: "\u{1F0A7}", value: 7 },
+//   { card: "\u{1F0A8}", value: 8 },
+//   { card: "\u{1F0A9}", value: 9 },
+//   { card: "\u{1F0AA}", value: 10 },
+//   { card: "\u{1F0AB}", value: 10 },
+//   { card: "\u{1F0AD}", value: 10 },
+//   { card: "\u{1F0AE}", value: 10 },
+//   { card: "\u{1F0B1}", value: 11 },
+//   { card: "\u{1F0B2}", value: 2 },
+//   { card: "\u{1F0B3}", value: 3 },
+//   { card: "\u{1F0B4}", value: 4 },
+//   { card: "\u{1F0B5}", value: 5 },
+//   { card: "\u{1F0B6}", value: 6 },
+//   { card: "\u{1F0B7}", value: 7 },
+//   { card: "\u{1F0B8}", value: 8 },
+//   { card: "\u{1F0B9}", value: 9 },
+//   { card: "\u{1F0BA}", value: 10 },
+//   { card: "\u{1F0BB}", value: 10 },
+//   { card: "\u{1F0BD}", value: 10 },
+//   { card: "\u{1F0BE}", value: 10 },
+//   { card: "\u{1F0C1}", value: 11 },
+//   { card: "\u{1F0C2}", value: 2 },
+//   { card: "\u{1F0C3}", value: 3 },
+//   { card: "\u{1F0C4}", value: 4 },
+//   { card: "\u{1F0C5}", value: 5 },
+//   { card: "\u{1F0C6}", value: 6 },
+//   { card: "\u{1F0C7}", value: 7 },
+//   { card: "\u{1F0C8}", value: 8 },
+//   { card: "\u{1F0C9}", value: 9 },
+//   { card: "\u{1F0CA}", value: 10 },
+//   { card: "\u{1F0CB}", value: 10 },
+//   { card: "\u{1F0CD}", value: 10 },
+//   { card: "\u{1F0CE}", value: 10 },
+//   { card: "\u{1F0D1}", value: 11 },
+//   { card: "\u{1F0D2}", value: 2 },
+//   { card: "\u{1F0D3}", value: 3 },
+//   { card: "\u{1F0D4}", value: 4 },
+//   { card: "\u{1F0D5}", value: 5 },
+//   { card: "\u{1F0D6}", value: 6 },
+//   { card: "\u{1F0D7}", value: 7 },
+//   { card: "\u{1F0D8}", value: 8 },
+//   { card: "\u{1F0D9}", value: 9 },
+//   { card: "\u{1F0DA}", value: 10 },
+//   { card: "\u{1F0DB}", value: 10 },
+//   { card: "\u{1F0DD}", value: 10 },
+//   { card: "\u{1F0DE}", value: 10 }
+// ];
 
 //a function to shuffle the deck
 function shuffle(deck) {
