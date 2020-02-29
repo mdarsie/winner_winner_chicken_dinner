1a8ca13b3cf7a584fa953cbd1fb981c5b64f6025
mdarsie <github@darsie.me>
2020-02-29 16:01:48 -0500

    add insurance rule, font from Spectral SC to Spectral

--- circle.js
+++ circle.js
@@ -3,3 +3,6 @@ circleType.radius(300).dir(-1);
 
 const circleTypeB = new CircleType(document.getElementById("softHit"));
 circleTypeB.radius(320).dir(-1);
+
+const circleTypeC = new CircleType(document.getElementById("insuranceRule"));
+circleTypeC.radius(330).dir(-1);
