4f7c77c8987601af25d1779c2e555e545b43540c
mdarsie <github@darsie.me>
2020-02-29 15:45:40 -0500

    align table rules, adjust colors and add text shadow

--- circle.js
+++ circle.js
@@ -1,5 +1,5 @@
 const circleType = new CircleType(document.getElementById("blackjackPay"));
-circleType.radius(150).dir(-1);
+circleType.radius(300).dir(-1);
 
 const circleTypeB = new CircleType(document.getElementById("softHit"));
-circleTypeB.radius(170).dir(-1);
+circleTypeB.radius(320).dir(-1);
