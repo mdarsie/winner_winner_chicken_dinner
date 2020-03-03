13e6fb162f565647fec5d858517609f2831f81c9
mdarsie <github@darsie.me>
2020-03-03 05:58:07 -0500

    new game function, deal hand function and calculate hand value function

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ class Deck {
   constructor() {
     this.length = 0;
     this.cards = [];
-    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+    const suits = ["H", "D", "S", "C"];
     const values = [
-      "Ace",
+      "A",
       "2",
       "3",
       "4",
@@ -49,9 +49,9 @@ class Deck {
       "8",
       "9",
       "10",
-      "Jack",
-      "Queen",
-      "King"
+      "J",
+      "Q",
+      "K"
     ];
     for (let i = 0; i < suits.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
@@ -78,57 +78,75 @@ function shuffle(deck) {
   return deck;
 }
 
+//beginning a new game
+function newGame() {
+  shuffle(deck);
+  playerHand = [];
+  dealerHand = [];
+  dealerScore.innerHTML += "?";
+  var myBet = document.getElementById("myBet").value;
+  chipStack = chipStack - myBet;
+  document.getElementById("chipStack").innerHTML += chipStack;
+  dealHand();
+}
+
+function dealHand(){
+  playerHand.push(deck.shift());
+  dealerHand.push(deck.shift());
+  playerHand.push(deck.shift());
+  dealerHand.push(deck.shift());
+  console.log("player: ", playerHand);
+  console.log("dealer:", dealerHand)
+}
+
+
+
 //a function to determine the values of the cards
-// function cardValue(card) {
-//   switch (card[0]) {
-//     case "2":
-//     case "3":
-//     case "4":
-//     case "5":
-//     case "6":
-//     case "7":
-//     case "8":
-//     case "9":
-//       return parseInt(card[0]);
-//     case "1":
-//     case "J":
-//     case "Q":
-//     case "K":
-//       return 10;
-//     default:
-//       return 11;
-//   }
-// }
+function cardValue(card) {
+  switch (card[1]) {
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      return parseInt(card[1]);
+    case "1":
+    case "J":
+    case "Q":
+    case "K":
+      return 10;
+    default:
+      return 11;
+  }
+}
 // // a function to determine the value of a hand
-// function getAce(card) {
-//   if (card[0] == "A") {
-//     return card;
-//   }
-// }
-// function handTotal(hand) {
-//   var i = 0;
-//   var aces = hand.filter(getAce);
-//   var handValue = 0;
-//   hand.forEach(function(card) {
-//     return (handValue += cardValue(card));
-//   });
-//   if (aces.length !== 0) {
-//     aces.forEach(function() {
-//       if (handValue > 21) {
-//         return (handValue -= 10);
-//       }
-//     });
-//   }
-//   return handValue;
-// }
+function getAce(card) {
+  if (card[1] == "A") {
+    return card;
+  }
+}
+function handTotal(hand) {
+  var i = 0;
+  var aces = hand.filter(getAce);
+  var handValue = 0;
+  hand.forEach(function(card) {
+    return (handValue += cardValue(card));
+  });
+  if (aces.length !== 0) {
+    aces.forEach(function() {
+      if (handValue > 21) {
+        return (handValue -= 10);
+      }
+    });
+  }
+  return handValue;
+}
 // //event listeners for game start, hit, stand, play again
 // newGameButton.addEventListener("click", function() {
 //   deck = shuffle();
-//   playerHand.push(deck.shift());
-//   dealerHand.push(deck.shift());
-//   playerHand.push(deck.shift());
-//   dealerHand.push(deck.shift());
-//   console.log("player: ", playerHand);
 
 //   displayCardsP(playerHand[0], playerCardsRow, true);
 //   displayCardsD(dealerHand[0], dealerCardsRow, true);
@@ -256,68 +274,3 @@ function shuffle(deck) {
 //   }
 // });
 
-// add values to array to create deck of cards
-// function shuffle() {
-//   let deck = [
-//     "2H",
-//     "3H",
-//     "4H",
-//     "5H",
-//     "6H",
-//     "7H",
-//     "8H",
-//     "9H",
-//     "10H",
-//     "JH",
-//     "QH",
-//     "KH",
-//     "AH",
-//     "2S",
-//     "3S",
-//     "4S",
-//     "5S",
-//     "6S",
-//     "7S",
-//     "8S",
-//     "9S",
-//     "10S",
-//     "JS",
-//     "QS",
-//     "KS",
-//     "AS",
-//     "2C",
-//     "3C",
-//     "4C",
-//     "5C",
-//     "6C",
-//     "7C",
-//     "8C",
-//     "9C",
-//     "10C",
-//     "JC",
-//     "QC",
-//     "KC",
-//     "AC",
-//     "2D",
-//     "3D",
-//     "4D",
-//     "5D",
-//     "6D",
-//     "7D",
-//     "8D",
-//     "9D",
-//     "10D",
-//     "JD",
-//     "QD",
-//     "KD",
-//     "AD"
-//   ];
-//   for (var i = 0; i < 500; i++) {
-//     var space1 = Math.floor(Math.random() * deck.length);
-//     var space2 = Math.floor(Math.random() * deck.length);
-//     var placeHolder = deck[space1];
-//     deck[space1] = deck[space2];
-//     deck[space2] = placeHolder;
-//   }
-//   return deck;
-// }
